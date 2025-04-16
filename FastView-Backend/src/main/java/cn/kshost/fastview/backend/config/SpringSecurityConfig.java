4055d7e6feee9a7392e579de2cc6b949781b4e21
Zzay <1840238422@qq.com>
2025-04-16 13:22:25 +0800

    new

--- FastView-Backend/src/main/java/cn/kshost/fastview/backend/config/SpringSecurityConfig.java
+++ FastView-Backend/src/main/java/cn/kshost/fastview/backend/config/SpringSecurityConfig.java
@@ -31,8 +31,6 @@ public class SpringSecurityConfig {
     @Autowired
     LoginAuthenticationEntryPoint loginAuthenticationEntryPoint;
 
-    @Autowired
-    LoginAuthenticationEntryPoint kk;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
